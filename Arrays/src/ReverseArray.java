5374d32a82578c08ffafd42339d2a73d90667bbf
Dimple Gupta <hunnyhhh1994@gmail.com>
2022-07-09 12:50:02 +0530

    First Commit

---
+++ Arrays/src/ReverseArray.java
@@ -0,0 +1,26 @@
+public class ReverseArray{
+
+    public void arrayReverse(int arr[], int start, int end){
+        while (start < end){
+            int temp =  arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void printArray(int arr[]){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        ReverseArray reverseArray = new ReverseArray();
+        reverseArray.arrayReverse(arr,0,arr.length-1);
+        reverseArray.printArray(arr);
+
+    }
+}
